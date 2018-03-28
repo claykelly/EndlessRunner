eb99f822a7127bd7e2e79539625042e8e9516888
Clay Kelly <cmk.clay@yahoo.com>
2018-03-27 20:01:29 -0500

    Start-Up
    Starting Code from Brennan

---
+++ Endless Runner/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Endless Runner
+//
+//  Created by Karl Karlson on 3/27/18.
+//  Copyright © 2018 Brennan Stewart. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
