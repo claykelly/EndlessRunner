1694cef6d25151897a3424cb114b4edbd9be1ecd
guckiant <trey.guckian@utexas.edu>
2018-04-10 13:19:14 -0500

    Gameplay Update
    Added gameplay. Affects GameScene.swift and .sks, GameViewController.swift and added sprite to assets folder.

--- Endless Runner/GameScene.swift
+++ Endless Runner/GameScene.swift
@@ -1,110 +1,79 @@
 //
 //  GameScene.swift
-//  ofggwekjf
+//  PracticeGame
 //
-//  Created by Clay Kelly on 3/27/18.
-//  Copyright © 2018 Clay Kelly. All rights reserved.
+//  Created by Trey Guckian on 4/9/18.
+//  Copyright © 2018 Trey Guckian. All rights reserved.
 //
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene{
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var orangeGuy = SKSpriteNode()
+    var groundNode = SKSpriteNode()
+    var backgroundNode = SKSpriteNode()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var rect: CGRect?
+    var rectWidth: CGFloat = 0.0
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    override func didMove(to view: SKView) {
+        setUp()
+        Timer.scheduledTimer(timeInterval: TimeInterval(2), target: self, selector: #selector(GameScene.makeWall), userInfo: nil, repeats: true)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    override func update(_ currentTime: TimeInterval) {
+        moveWalls()
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        orangeGuy.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60))
+    }
+    
+    func randomWidth(_ range:Range<Int>) -> CGFloat
+    {
+        let rndm = Int32(arc4random_uniform(UInt32(range.upperBound - range.lowerBound)))
+        let randCGF = CGFloat(rndm)
+        return CGFloat(range.lowerBound) + randCGF
+    }
+    
+    func setUp(){
+        orangeGuy = self.childNode(withName: "OrangeGuy") as! SKSpriteNode
+        orangeGuy.physicsBody = SKPhysicsBody(circleOfRadius: orangeGuy.size.width/2)
+        groundNode = self.childNode(withName: "GroundNode") as! SKSpriteNode
+        backgroundNode = self.childNode(withName: "BackgroundNode") as! SKSpriteNode
+        
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.backgroundNode.frame)
+
+    }
+    
+    @objc func makeWall(){
+        var rectWidth = CGFloat()
+        var rectHeight = CGFloat()
+        
+        rectWidth = CGFloat(randomWidth(37..<55))
+        rectHeight = CGFloat(randomWidth(63..<85))
+        
+        let wallNode = SKShapeNode(rectOf: CGSize(width: rectWidth, height: rectHeight))
+        wallNode.position.x = groundNode.frame.maxX + wallNode.frame.size.width
+        wallNode.position.y = groundNode.frame.maxY + wallNode.frame.size.height/2 - 1
+        wallNode.zPosition = 10.0
+        wallNode.strokeColor = SKColor.magenta
+        wallNode.fillColor = SKColor.magenta
+        wallNode.name = "wallNode"
+        self.addChild(wallNode)
+    }
+    
+    func moveWalls(){
+        self.enumerateChildNodes(withName: "wallNode") {
+            node, stop in
+            // do something with node or stop
+            node.position.x -= 10
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
 }
+
+
