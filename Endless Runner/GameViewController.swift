5f7daa573975a32c04e35b90fee6c844222d74e9
Clay Kelly <cmk.clay@yahoo.com>
2018-04-10 15:01:56 -0500

    Reverting back to "Firebase, log in, log out push"
    Git hub messed up so we are reverting back to the original push.

--- Endless Runner/GameViewController.swift
+++ Endless Runner/GameViewController.swift
@@ -1,9 +1,9 @@
 //
 //  GameViewController.swift
-//  PracticeGame
+//  ofggwekjf
 //
-//  Created by Trey Guckian on 4/9/18.
-//  Copyright © 2018 Trey Guckian. All rights reserved.
+//  Created by Clay Kelly on 3/27/18.
+//  Copyright © 2018 Clay Kelly. All rights reserved.
 //
 
 import UIKit
@@ -15,20 +15,30 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                sceneNode.scaleMode = .aspectFill
                 
                 // Present the scene
-                view.presentScene(scene)
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
     }
 
