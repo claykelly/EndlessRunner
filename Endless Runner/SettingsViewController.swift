86577dd40df7bd078dbff404d4e52e5bfe702096
Clay Kelly <cmk.clay@yahoo.com>
2018-04-30 01:51:22 -0500

    Redo Constraints

--- Endless Runner/SettingsViewController.swift
+++ Endless Runner/SettingsViewController.swift
@@ -27,7 +27,7 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let newScore = addNewScore.create(_newScore: 0)
-        newScore.addNew()
+        newScore.addNew()        
     }
     
     override func didReceiveMemoryWarning() {
